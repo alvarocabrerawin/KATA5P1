ad0b9d921431324b2e2dd701b1fc22586c2dc19b
alvar <alvar@DESKTOP-52OFONO>
2021-12-09 12:24:24 +0000

    Kata5p1 Version2

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -1,43 +1,27 @@
 package kata5;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 public class Kata5 {
-// Se conecta a la BD y se devuelve un objeto Connection
-private Connection connect() {
-// SQLite connection string
-String url = "jdbc:sqlite:C:\\sql\\kata_5.db";
-Connection conn = null;
-try {
-conn = DriverManager.getConnection(url);
+public static void createNewTable() {
+// Cadena de conexión SQLite
+String url = "jdbc:sqlite:C:\\sql\\kata5p1_v2.db";
+// Instrucción SQL para crear nueva tabla
+String sql = "CREATE TABLE IF NOT EXISTS direcc_email (\n"
++ " id integer PRIMARY KEY AUTOINCREMENT,\n"
++ " direccion text NOT NULL);";
+try (Connection conn = DriverManager.getConnection(url);
+Statement stmt = conn.createStatement()) {
+// Se crea la nueva tabla
+stmt.execute(sql);
+System.out.println("Tabla creada");
 } catch (SQLException e) {
 System.out.println(e.getMessage());
-}
-return conn;
-}
-
-// Se seleccionan todos los registros de la tabla PEOPLE
-public void selectAll(){
-String sql = "SELECT * FROM PEOPLE";
-try (Connection conn = this.connect();
-Statement stmt = conn.createStatement();
-ResultSet rs = stmt.executeQuery(sql)){
-// Bucle sobre el conjunto de registros.
-while (rs.next()) {
-System.out.println(rs.getInt("Id") + "\t" + 
-rs.getString("Name") + "\t" +
-rs.getString("Apellidos") + "\t" +
-rs.getString("Departamento") + "\t");
-}
-} catch (SQLException e) {
-System.out.println(e.getMessage());
-}
+} 
 }
 public static void main(String[] args) {
-Kata5 app = new Kata5();
-app.selectAll();
+createNewTable();
 }
 }
 
